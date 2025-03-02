233869a06b9bf30a02a8d6e37c461a514c223366
Magda <wozna.magda@gmail.com>
2025-03-02 19:54:45 +0100

    feat(result): add hardcoded result component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <GeoDistanceForm />
+    <GeoDistanceResult :distance="{ km: 12, m: 10 }" />
   </div>
 </template>
 
 <script setup lang="ts">
 import GeoDistanceForm from './components/GeoDistanceForm.vue'
+import GeoDistanceResult from './components/GeoDistanceResult.vue'
 </script>
---
+++ frontend/src/components/GeoDistanceResult.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="distance">
+    <h3>Wynik:</h3>
+    <p>Odległość: {{ distance.km }} km {{ distance.m }} m</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  distance: Object,
+})
+</script>
