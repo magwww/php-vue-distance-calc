52f84a52fe8c49e79ebb564a716694bf0918da5e
Magda <wozna.magda@gmail.com>
2025-03-03 15:22:24 +0100

    styles( result-component): display fix

--- frontend/src/components/GeoDistanceResult.vue
+++ frontend/src/components/GeoDistanceResult.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="distance">
+  <div v-if="distance" class="result-container">
     <h3>
       Result: <span>{{ distance.km }} km {{ distance.m }} m</span>
     </h3>
@@ -11,3 +11,13 @@ defineProps({
   distance: Object,
 })
 </script>
+
+<style scoped>
+.result-container {
+  text-align: center;
+
+  span {
+    display: block;
+  }
+}
+</style>
