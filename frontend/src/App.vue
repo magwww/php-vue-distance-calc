c0c1ab443f151fcdcf6a578c99cb74512337cf02
Magda <wozna.magda@gmail.com>
2025-03-03 15:09:52 +0100

    feat(home): add start animation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container animate-in">
     <h1>Calculate distance</h1>
     <GeoDistanceForm @calculate="calculateDistance" />
     <GeoDistanceResult :distance="distance" />
@@ -28,5 +28,28 @@ const calculateDistance = async (points: { point1: GeoPoint; point2: GeoPoint })
   flex-direction: column;
   gap: 2rem;
   align-items: center;
+  opacity: 0;
+}
+
+@keyframes bounceIn {
+  0% {
+    transform: translateY(30px);
+    opacity: 0;
+  }
+  70% {
+    transform: translateY(-8px);
+    opacity: 1;
+  }
+  85% {
+    transform: translateY(4px);
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+.animate-in {
+  animation: bounceIn 0.8s cubic-bezier(0.36, 0, 0.66, -0.56) forwards;
 }
 </style>
