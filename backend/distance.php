b703bd99b3fe9039dac6902b3fac270a0959cb29
Magda <wozna.magda@gmail.com>
2025-03-06 20:12:07 +0100

    fix(req): check if request_method exists

--- backend/distance.php
+++ backend/distance.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
 }
